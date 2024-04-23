d2b7127183a019fed74070d649a9cd9539033fad
Mark Baek <markbaek@marks-air-3.mynetworksettings.com>
2024-04-22 17:24:19 -0700

    button component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Button from "./components/Button/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Button color={"red"} shape={"square"}></Button>
     </>
   )
 }
