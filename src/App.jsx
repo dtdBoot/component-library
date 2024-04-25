02633f5bdf485b8499be15ac6379df4e43fb89a5
Mark Baek <markbaek@marks-air-3.mynetworksettings.com>
2024-04-24 17:11:42 -0700

    almost completed Testimonial component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import Button from "./components/Button/Button";
 import Banner from "./components/Banner/Banner";
 import Card from "./components/Card/Card";
 import Testimonials from "./components/Testimonials/Testimonials";
+import profilePic from "/src/assets/profile-pic-lg.png"
 
 function App() {
 
   return (
     <>
-      <Button color="gray" shape="pill"></Button>
+      {/* <Button color="gray" shape="pill"></Button>
       <Banner bannerType="error">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum</Banner>
-      <Card>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis. </Card>
+      <Card>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis. </Card> */}
       <Testimonials
         name="May Andersons"
         title="Workcation, CTO"
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,10 +2,45 @@ import React from "react";
 import doubleQuotes from "../../assets/double-quotes.svg";
 import dotPattern from "../../assets/dot-pattern.svg";
 import profilePicDefaultSmall from "../../assets/profile-pic.png";
-import profilePicDefaultLarge from "../../assets/profile-pic-lg.png"
+import profilePicDefaultLarge from "../../assets/profile-pic-lg.png";
+import defaultLogo from "../../assets/default-logo.svg";
+import slash from "../../assets/slash.svg";
 import "./Testimonials.css";
 
-export default function Testimonials({ profilePic = profilePicDefaultLarge, name, title, orgIcon, children }) {
+export default function Testimonials({ profilePic, name, title, orgIcon, children }) {
+
+    function testimonialBodyLayout() {
+
+        const testimonialBodyPicEl = (
+            <div className="testimonial-body-pic">
+                <img src={doubleQuotes} alt="An icon of double quotation marks." />
+                <div className="testimonial-text-container">
+                    <p className="testimonial-body-text">{children}</p>
+                    <div className="testimonial-source">
+                        <p className="testimonial-source-name">{name}</p>
+                        <p>{title}</p>
+                    </div>
+                </div>
+             </div>
+        )
+
+        const testimonialBodyNoPicEl = (
+            <div className="testimonial-body-no-pic">
+                <img className="logo-no-pic" src={defaultLogo} alt="An icon of double quotation marks." />
+                <div className="testimonial-text-container">
+                    <p className="testimonial-body-text">{children}</p>
+                    <div className="testimonial-source">
+                        <p className="testimonial-source-name">{name}</p>
+                        <img className="slash-logo" src={slash} alt="A slash logo" />
+                        <p>{title}</p>
+                    </div>
+                </div>
+                <img className="dot-pattern" src={dotPattern} alt="A decorative pattern of dots." />
+             </div>
+        )
+
+        return profilePic ? testimonialBodyPicEl : testimonialBodyNoPicEl;
+    }
 
     const profilePicEl = (
         <div 
@@ -20,15 +55,8 @@ export default function Testimonials({ profilePic = profilePicDefaultLarge, name
 
     return (
         <div className="testimonial-container">
-            {profilePicEl}
-            <div className="testimonial-body">
-                <img src={doubleQuotes} alt="An icon of double quotation marks." />
-                <p className="testimonial-body-text">{children}</p>
-                <div className="testimonial-source">
-                    <p className="testimonial-source-name">{name}</p>
-                    <p>{title}</p>
-                </div>
-            </div>
+            {profilePic && profilePicEl}
+            {testimonialBodyLayout()}
         </div>
     )
-}
+};
